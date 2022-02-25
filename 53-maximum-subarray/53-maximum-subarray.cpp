7caf01df60e69266d05321f317541ead7240faf6
Suvrodeep <81421963+SynTax19@users.noreply.github.com>
2022-02-25 18:06:09 +0530

    Time: 108 ms (82.33%), Space: 67.6 MB (91.27%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      
+        int m1 = 0, m2=INT_MIN , m3=INT_MIN;
+        
+        for (int i=0;i<nums.size();i++){
+            m3=max(m3,nums[i]);
+            
+            m1 = max(nums[i] , m1+nums[i]);
+            m2= max(m2,m1);
+            
+        }
+        return max(m2,m3);
+    }
+};
