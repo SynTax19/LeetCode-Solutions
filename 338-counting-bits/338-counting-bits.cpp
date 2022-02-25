ee9fbd16e3e93b53b0f220ab408d24aefbbdda94
Suvrodeep <81421963+SynTax19@users.noreply.github.com>
2022-02-25 19:45:14 +0530

    Time: 15 ms (12.87%), Space: 8.5 MB (21.50%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -5,10 +5,11 @@ public:
         vector <int> v; int i=0;
         for (int i=0;i<=n;i++){
             
-            int x = i , c=0 , j=0;
+            int x = i , c=0 , j=1;
             while(x){
-                if (x & 1){ c++; }
-                x/=2;
+               
+                if (x & j){ c++; }
+                x = x>>1;
             }
             v.push_back(c);
         }
