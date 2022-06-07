72d68dfc94bebad88dac046c1a4033a89948d19d
Suvrodeep <81421963+SynTax19@users.noreply.github.com>
2022-06-07 19:36:22 +0530

    Time: 27 ms (56.24%), Space: 17.9 MB (64.48%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         
-        int mx=-1 , sum = 0 , sumreq=0;
+        // 1st Approach
+//         int mx = -1 , sum = 0 , sumreq=0;
         
-        for (int i=0;i<nums.size();i++) {
-            sum+= nums[i]; mx = max(mx,nums[i]);
-            sumreq+=i;
+//         for (int i=0;i<nums.size();i++) {
+//             sum+= nums[i]; mx = max(mx,nums[i]);
+//             sumreq+=i;
+//         }
+//         if (sum == sumreq) return mx+1;
+//         else {
+//            return mx - (sum - sumreq); 
+//         }
+        
+        // 2nd Approach 
+        
+        int xx =0;
+        for (int i=0;i<nums.size();i++){
+            xx = xx^nums[i]; xx^=i;
         }
-        if (sum == sumreq) return mx+1;
-        else {
-           return mx - (sum - sumreq); 
-        }
+        xx = xx^nums.size();
+        return xx;
+        
         
     }
 };
