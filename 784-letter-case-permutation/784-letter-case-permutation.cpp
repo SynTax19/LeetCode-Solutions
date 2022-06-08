7bd5c0b6760c60bc661c5d167fdd8d087b16f18b
Suvrodeep <81421963+SynTax19@users.noreply.github.com>
2022-06-08 09:53:29 +0530

    Time: 23 ms (10.75%), Space: 11.4 MB (51.21%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> letterCasePermutation(string s) {
+        vector<string> res;
+        set<string> temp;
+        int n = s.size();
+        for (int i=0;i<(1<<n);i++){
+            
+            string ss="";
+            for (int j=0;j<n;j++){
+                
+                if ((i & (1<<j)) && (s[j] >='a' && s[j]<='z'))
+                    ss.push_back(toupper(s[j]));
+                else if ((i & (1<<j)) && (s[j] >='A' && s[j]<='Z'))
+                    ss.push_back(tolower(s[j]));
+                else 
+                    ss.push_back(s[j]);  
+            }
+            temp.insert(ss);
+        }
+        for (auto it : temp){
+            res.push_back(it);
+        }
+        return res;
+    }
+};
