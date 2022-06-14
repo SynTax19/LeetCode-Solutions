82ff24cc515ad53ff3aed2378cde3564e81219b6
Suvrodeep <81421963+SynTax19@users.noreply.github.com>
2022-06-14 22:33:13 +0530

    Time: 21 ms (22.45%), Space: 11.6 MB (91.78%) - LeetHub

---
+++ 372-super-pow/372-super-pow.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int power (int x , int y){
+        
+        int mod = 1337 , res=1;
+        for (int i=0;i<y;i++){  res = ((res%mod)*(x%mod))%mod;  }
+        
+        return res;   
+        
+    }
+    
+    int superPow(int a, vector<int>& b) {
+       
+        int res=1 , temp=1 , mod = 1337;
+        
+        for (int i=0;i<b.size();i++){
+            
+            int x = power ( a, b[i] );
+            res = (res*x)%mod;
+            
+            temp = res; int y  = power ( res, 10 );
+            
+            res = (y)%mod; 
+        }
+        return temp;
+        
+    }
+    
+  
+    
+    
+};
